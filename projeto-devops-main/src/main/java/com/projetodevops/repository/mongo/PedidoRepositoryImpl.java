4600cba1688ba0d3305ac02c14e70c42fb8e463a
VinnyBurigatto <vinicius.burigatto@hotmail.com>
2026-03-06 18:46:19 -0300

    Continuação dos Estudos
    Desenvolvimento do PedidoRepositoryImpl.java

--- projeto-devops-main/src/main/java/com/projetodevops/repository/mongo/PedidoRepositoryImpl.java
+++ projeto-devops-main/src/main/java/com/projetodevops/repository/mongo/PedidoRepositoryImpl.java
@@ -1,5 +1,28 @@
 package com.projetodevops.repository.mongo;
 
-public class PedidoRepositoryImpl {
+import org.springframework.stereotype.Repository;
+
+import com.projetodevops.domain.Pedido;
+import com.projetodevops.domain.PedidoRepository;
+import com.projetodevops.repository.document.PedidoDocument;
+import com.projetodevops.repository.mapper.PedidoMapper;
+
+
+@Repository
+public class PedidoRepositoryImpl implements PedidoRepository {
+    private final PedidoMongoRepository pedidoMongoRepository;
+    private final PedidoMapper pedidoMapper;
+
+    public PedidoRepositoryImpl(PedidoMongoRepository pedidoMongoRepository, PedidoMapper pedidoMapper) {
+        this.pedidoMongoRepository = pedidoMongoRepository;
+        this.pedidoMapper = pedidoMapper;
+    }
+
+@Override
+    public Pedido salvar(Pedido pedido) {
+        PedidoDocument document = pedidoMapper.toDocument(pedido);
+        PedidoDocument savedDocument = pedidoMongoRepository.save(document);
+            return pedidoMapper.toDomain(savedDocument);
+    }
 
 }
